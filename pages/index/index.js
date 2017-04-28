9bd6c394cb0f59d524787bed281064124b826432
唐菊 <635465547@qq.com>
2017-04-28 14:31:00 +0800

    目录选中

--- pages/index/index.js
+++ pages/index/index.js
@@ -7,22 +7,22 @@ Page({
     activeIndex: 0,
     sliderOffset: 0,
     lists: [
-            {name:'鲁菜1',nub:0},
-            {name:'鲁菜2',nub:0},
-            {name:'鲁菜3',nub:0},
-            {name:'鲁菜4',nub:0},
-            {name:'鲁菜5',nub:0},
-            {name:'鲁菜6',nub:0},
-            {name:'鲁菜7',nub:0},
-            {name:'鲁菜8',nub:0},
-            {name:'鲁菜3',nub:0},
-            {name:'鲁菜4',nub:0},
-            {name:'鲁菜5',nub:0},
-            {name:'鲁菜6',nub:0},
-            {name:'鲁菜7',nub:0},
-            {name:'鲁菜8',nub:0},
+            {name:'鲁菜1',nub:1},
+            {name:'鲁菜2',nub:2},
+            {name:'鲁菜3',nub:3},
+            {name:'鲁菜4',nub:4},
+            {name:'鲁菜5',nub:5},
+            {name:'鲁菜6',nub:6},
+            {name:'鲁菜7',nub:7},
+            {name:'鲁菜8',nub:8},
+            {name:'鲁菜3',nub:9},
+            {name:'鲁菜4',nub:10},
+            {name:'鲁菜5',nub:11},
+            {name:'鲁菜6',nub:12},
+            {name:'鲁菜7',nub:13},
+            {name:'鲁菜8',nub:14},
         ],
-    currentIndex:0,
+    currentType:1,
     userInfo: {},
   },
   //事件处理函数
@@ -41,6 +41,12 @@ Page({
         this.setData({
             sliderOffset: e.currentTarget.offsetLeft,
             activeIndex: e.currentTarget.id
+        })
+    },
+  selectType:function(e){
+    console.log('hiojo',e.currentTarget.dataset)
+    this.setData({
+            currentType: e.currentTarget.dataset.index
         });
-    }
+  },
 })
